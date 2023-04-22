a071b7b58a9cf85245975b9fc4bfcf9cbf40f20b
MayconRafael <mayconrafacruz@gmail.com>
2023-04-22 11:55:36 -0300

    Atualização login e token

--- models/User.js
+++ models/User.js
@@ -2,13 +2,20 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false},
     isAdmin: { type: Boolean, required: false },
     isSendEmail: { type: Boolean, required: false },
 });
+
+userSchema.pre("save", async function(next) {
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
 
 const User = mongoose.model("User", userSchema);
 
