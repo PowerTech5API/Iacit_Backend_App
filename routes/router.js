81bec4f28c01706ca9d8fabc4d435c8cb8a365a1
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-04-23 11:39:59 -0300

    controller de termo

--- routes/router.js
+++ routes/router.js
@@ -4,10 +4,12 @@ const userRouter = require("./user")
 const roRouter = require("./ro")
 const chatRouter = require("./chat")
 const configRouter = require("./config")
+const termsRouter = require("./terms")
 
 router.use("/", userRouter)
 router.use("/", roRouter)
 router.use("/", chatRouter)
 router.use("/", configRouter)
+router.use("/", termsRouter)
 
 module.exports = router;
